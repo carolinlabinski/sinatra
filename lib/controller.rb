7d4d19dca2ee7f2a1a5665bb927752eee5945069
carolin.labinski <58142571+carolinlabinski@users.noreply.github.com>
2020-02-03 23:23:33 +0100

    Add files via upload

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,30 +2,22 @@ require 'gossip'
 
 class ApplicationController < Sinatra::Base
 
-get '/' do
- erb :index, locals: {gossips: Gossip.all}
-end
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
 
-get '/gossips/new/' do
- erb :new_gossip
-end
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
 
-post '/gossips/new/' do
- Gossip.new(params["gossip_author"],params["gossip_content"]).save
- redirect '/'
-end
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"],params["gossip_content"]).save
+    redirect '/'
+  end
 
-# get '/gossips/:id' do
-# id = params['id'].to_i
-# erb :show, locals:{gossips: Gossip.find(id), index: id}
-# Gossip.find(id)
-# end
-
-get '/gossips/:id/' do
+  get '/gossips/:id/' do
     puts "Voici le numéro du potin que tu veux : #{params['id']} !"
     erb :show, locals: {id: params['id'].to_i, gossips: Gossip.find(params['id'].to_i)}
   end
-
-
 
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -4,54 +4,36 @@ class Gossip
 attr_reader :author, :content 
     
     
-def initialize (author,content)
+  def initialize (author,content)
     @author = author
     @content = content
- end
-
-
-def save
-    CSV.open("./db/gossip.csv","ab") do |csv|
-    csv << [@author,@content]
-    end
-end
-
- def self.all
-  all_gossips = []
-  CSV.read("./db/gossip.csv").each do |csv_line|
-    all_gossips << Gossip.new(csv_line[0],csv_line[1])
   end
+
+
+  def save
+    CSV.open("./db/gossip.csv","ab") do |csv|
+     csv << [@author,@content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+     all_gossips << Gossip.new(csv_line[0],csv_line[1])
+    end
    return all_gossips
- end
+  end
 
 
- def self.find(id)
+   def self.find(id)
     gossips = []
     CSV.read("./db/gossip.csv").each_with_index do |row,index|
-        if id == index+1 #cherche et check si l'index est égale à l'id demandé
-            gossips << Gossip.new(row[0], row[1]) #si trouvé, ajout dans array et break pour retourner l'array
-            break    
-            end    
-          end    
-          return gossips   
-        end
-   
-       
-
-    # go.each do |line|
-    #     i += 1
-    #     unless i == choice # On compare si on arrive `a la ligne choisie
-    #         CSV.open("db/gossip.csv","a") do |row, index|
-    #             if id == index+1 #cherche et check si l'index est égale à l'id demandé
-    #                 gossips << Gossip.new(row[0], row[1]) #si trouvé, ajout dans array et break pour retourner l'array
-    #                 break    
-    #                 end    
-    #               end    
-    #               return gossips   
-    #             end
- 
-
- 
-
+       if id == index+1 #cherche et check si l'index est égale à l'id demandé
+        gossips << Gossip.new(row[0], row[1]) #si trouvé, ajout dans array et break pour retourner l'array
+       break    
+       end    
+      end    
+    return gossips   
+   end
 end
 
